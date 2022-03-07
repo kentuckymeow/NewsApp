830181038a20002c4822a6aeade5e47223a242e4
ArseniKhatsuk <khatsuk007@gmail.com>
2022-03-07 17:37:50 +0300

    -update Main -add Cocoa Touch Class (DetailViewController) with the web view -add the TableView and the PrototypeCell -set the tableView as Delegate and DataSource

---
+++ News/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  News
+//
+//  Created by Arseni Khatsuk on 07.03.2022.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- News/ViewController.swift
+++ News/ViewController.swift
@@ -9,14 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     var model = ArticleModel()
     var articles = [Article]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set the viewcontroller as the datasource and delegate of the table view
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Get the articles from the article model
         model.delegate = self
         model.getArticles()
+    }
+    
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath)
+        
+        // TODO: customize it
+        
+        // Return the cell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
     
 }
@@ -29,6 +60,9 @@ extension ViewController:  ArticleModernProtocol{
         
         // Set the articles property of the view controller to the articless passed back from the model 
         self.articles = article
+        
+        // Refresh the tableview
+        tableView.reloadData()
     }
     
 }
